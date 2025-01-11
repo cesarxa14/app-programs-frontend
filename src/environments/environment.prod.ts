8c8fc1e4d15d47d3484dfaeeea28e08a9ababd70
MutsuM <marko.pachas@gmail.com>
2025-01-10 23:48:52 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URI: 'https://167.88.43.130:3000/'
+  //API_URI: 'https://167.88.43.130:3000/'
+  API_URI: 'https://clientes.nadaresvidaperu.pe/'
 };
